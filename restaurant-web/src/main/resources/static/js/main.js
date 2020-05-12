90f59d132356489edba125814793ba88baa82744
labtec96 <cezaryhan1@gmail.com>
2020-05-12 15:24:22 +0200

    added welcome page

--- restaurant-web/src/main/resources/static/js/main.js
+++ restaurant-web/src/main/resources/static/js/main.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("openMenu").innerHTML = "";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("openMenu").innerHTML = "☰";
 }
