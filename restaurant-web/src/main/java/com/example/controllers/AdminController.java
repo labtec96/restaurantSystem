b200e812ea1af40daabbe2e35e9ffc6b7d631dff
labtec96 <cezaryhan1@gmail.com>
2020-05-14 18:08:54 +0200

    added admin site

---
+++ restaurant-web/src/main/java/com/example/controllers/AdminController.java
@@ -0,0 +1,45 @@
+package com.example.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by ch on 2020-05-14
+ */
+@Slf4j
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        return "admin/admin";
+    }
+
+    @GetMapping("/admin/report")
+    public String report(Model model) {
+        return "report";
+    }
+
+    @GetMapping("/admin/table")
+    public String table(Model model) {
+        return "table";
+    }
+
+    @GetMapping("/admin/worker")
+    public String worker(Model model) {
+        return "worker";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String reservation(Model model) {
+        return "reservation";
+    }
+
+    @GetMapping("/admin/menu")
+    public String menu(Model model) {
+        return "menu";
+    }
+
+}
